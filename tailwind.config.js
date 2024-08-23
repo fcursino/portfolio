16ca953f9ba26e013bc0a3f6409a8eb0ffd38611
fcursino <fabriciocursino09@gmail.com>
2024-08-23 07:39:40 -0300

    defining site colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: 'rgb(190 231 255)',
+          dark: 'rgb(6 72 147)',
+        },
+        secondary: {
+          light: 'rgb(232 242 253)',
+          dark: 'rgb(9 13 33)',
+        },
+        accent: {
+          light: 'rgb(201 229 245)',
+          dark: 'rgb(36 38 41)',
+        },
+      },
       fontFamily: {
         sans: ['Noto']
       }
