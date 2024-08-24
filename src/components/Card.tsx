6b6830250eacc32e6116f95ec3a5378aa24db7c9
Fabricio Cursino <fabriciocursino09@gmail.com>
2024-08-24 18:02:39 +0000

    creating the structuring and modeling de style of new card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,8 @@
+const Card: React.FC = () => {
+  return (
+    <div className="shadow-md max-w-full w-full m-10 p-4 border border-accent-dark dark:border-accent-light rounded-lg">
+        <h1>aaa</h1>
+    </div>
+  )
+}
+export default Card
