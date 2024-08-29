a8827a8beb35e13aa372888662730a4d5f2583f4
fcursino <fabriciocursino09@gmail.com>
2024-08-29 08:07:40 -0300

    implementing icons management config

---
+++ src/config/iconConfig.ts
@@ -0,0 +1,13 @@
+import css from '../assets/icons/css.svg'
+import git from '../assets/icons/git.svg'
+import github from '../assets/icons/github.svg'
+import html from '../assets/icons/html.svg'
+import javascript from '../assets/icons/javascript.svg'
+import mysql from '../assets/icons/mysql.svg'
+import node from '../assets/icons/node.svg'
+import php from '../assets/icons/php.svg'
+import python from '../assets/icons/python.svg'
+import react from '../assets/icons/react.svg'
+import vue from '../assets/icons/vue.svg'
+
+export default { css, git, github, html, javascript, mysql, node, php, python, react, vue }
